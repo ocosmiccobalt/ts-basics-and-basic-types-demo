88745a022b821295597ee4ab0787f84d9ca3bb48
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-06-10 08:17:22 +0300

    Work with enums

--- app.ts
+++ app.ts
@@ -2,16 +2,29 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Maximilian',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 5, READ_ONLY, AUTHOR };
+
+const person = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 };
 
 // Push actually is an exeption which is allowed in tuples:
@@ -29,4 +42,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   // console.log(hobby.map()); // Error!
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
 }
