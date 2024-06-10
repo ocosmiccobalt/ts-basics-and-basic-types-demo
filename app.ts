d99050f48571b60a59aa1096280ea30f5b4bd8f6
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-06-10 10:26:03 +0300

    Use 'void'

--- app.ts
+++ app.ts
@@ -1,43 +1,12 @@
-type User = { name: string; age: number };
-const u1: User = { name: 'Max', age: 30 };
-
-function greet(user: User) {
-  console.log('Hi, I am' + user.name);
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-function isOlder(user: User, checkAge: number) {
-  return checkAge > user.age;
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+  return;
 }
 
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
+printResult(add(5, 12));
 
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  return result;
-
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
-}
-
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+// let someValue: undefined;
---
+++ union-aliases.ts
@@ -0,0 +1,43 @@
+type User = { name: string; age: number };
+const u1: User = { name: 'Max', age: 30 };
+
+function greet(user: User) {
+  console.log('Hi, I am' + user.name);
+}
+
+function isOlder(user: User, checkAge: number) {
+  return checkAge > user.age;
+}
+
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+
+  return result;
+
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
+}
+
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
