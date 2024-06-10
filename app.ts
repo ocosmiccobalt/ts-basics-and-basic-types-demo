d1cbc818e54eb603081c68f3a348cb0e250a31cb
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-06-10 09:20:42 +0300

    Work with type aliases (custom types)

--- app.ts
+++ app.ts
@@ -1,7 +1,21 @@
+type User = { name: string; age: number };
+const u1: User = { name: 'Max', age: 30 };
+
+function greet(user: User) {
+  console.log('Hi, I am' + user.name);
+}
+
+function isOlder(user: User, checkAge: number) {
+  return checkAge > user.age;
+}
+
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: 'as-number' | 'as-text'
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
 ) {
   let result;
   if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
