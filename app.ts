b4cec51640ad0752a177b8be186f386ef9a63c9c
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-06-10 11:13:15 +0300

    Use 'never' type

--- app.ts
+++ app.ts
@@ -7,3 +7,13 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+// This function never produces a value, never returns anything.
+// It always crashes the script (or this part of the script):
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  // or:
+  // while (true) {}
+}
+
+generateError('An error occured!', 500);
